09b953227e14f6c524f37b6595f43181599feadf
Jerome <derooverjdr@gmail.com>
2023-10-04 16:22:31 +0200

    initial wip

---
+++ app.js
@@ -0,0 +1,6 @@
+let form = document.querySelector(".form");
+let errorMessages = document.querySelectorAll(".msg");
+let errorMessageName = errorMessages[0];
+let errorMessageMail = errorMessages[1];
+let errorMessagePassword = errorMessages[2];
+let errorMessageConfirmPassword = errorMessages[3];
